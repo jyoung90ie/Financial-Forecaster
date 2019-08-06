10d4e87d0e3859dd2dea40f88749e712f3e3dbdb
John Y <jyoung90ie@gmail.com>
2019-08-06 22:24:54 +0100

    Form text and number validation added

--- js/main.js
+++ js/main.js
@@ -4,23 +4,35 @@ let startTab = 0; // first tab = 0
 
 let tabIdentifier = 'tab'; // word which identifies all tabs
 
-initTab(startTab);
+const getActiveTab = () => document.querySelectorAll('section[id*="' + tabIdentifier + '"]:not(.hide)')[0];
 
-function getActiveTab() {
-    return document.querySelectorAll('section[id*="' + tabIdentifier + '"]:not(.hide)')[0];;
-}
+const getTabs = () => document.querySelectorAll('section[id*="' + tabIdentifier + '"]');
 
-function getTabs() {
-    return document.querySelectorAll('section[id*="' + tabIdentifier + '"]');
-}
+const pushValuesToArray = (array, ref, elementName, elementAttribute, count) => {
+    array.push({
+        "ref": ref,
+        "prefix": elementName,
+        "attribute": elementAttribute,
+        "count": count,
+        "current": 1
+    });
+};
+
+const getAttributePrefix = attributeValue => {
+    if (typeof (attributeValue) == "string") {
+        let splitStringPosition = parseInt(attributeValue.lastIndexOf('-')) + 1;
+
+        return attributeValue.substr(0, splitStringPosition);
+    }
+};
 
 function initTab(startTab) {
     // function which will set the current tab
-    var allTabs = getTabs();
-    var getTabIndicators = document.querySelectorAll('#progress-indicator span');
-    var numberOfTabs = allTabs.length;
+    let allTabs = getTabs();
+    let getTabIndicators = document.querySelectorAll('#progress-indicator span');
+    let numberOfTabs = allTabs.length;
 
-    for (i = 0; i < numberOfTabs; i++) {
+    for (let i = 0; i < numberOfTabs; i++) {
         if (i === startTab) {
             allTabs[i].classList.remove('hide');
             getTabIndicators[i].classList.add('active');
@@ -33,29 +45,30 @@ function initTab(startTab) {
 
 
 function navigateTabs(type) {
-    var activeTab = getActiveTab();
-    var arrayOfTabs = getTabs();
+    let activeTab = getActiveTab();
+    let tabs = getTabs();
 
-    var getTabIndicators = document.querySelectorAll('#progress-indicator span');
+    let getTabIndicators = document.querySelectorAll('#progress-indicator span');
 
-    arrayOfTabs.forEach(function (item, index) {
+    tabs.forEach((tab, index) => {
 
-        var currentTabNumber = index + 1; // need to add one as first array item has index of 0
-        var numberOfTabs = arrayOfTabs.length; // length of array indicates number of tabs
+        let currentTabNumber = index + 1; // need to add one as first array item has index of 0
+        let numberOfTabs = tabs.length; // length of array indicates number of tabs
 
         // loop through all elements that match query selector and only continue for the current active tab
-        if (item.id === activeTab.id) {
+        if (tab.id === activeTab.id) {
+            let adjTabNumber;
             if (type === 'next') {
-                arrayOfTabs[index + 1].classList.remove("hide");
-                var adjTabNumber = currentTabNumber + 1;
+                tabs[index + 1].classList.remove('hide');
+                adjTabNumber = currentTabNumber + 1;
             } else if (type === 'prev') {
-                arrayOfTabs[index - 1].classList.remove("hide");
-                var adjTabNumber = currentTabNumber - 1;
+                tabs[index - 1].classList.remove('hide');
+                adjTabNumber = currentTabNumber - 1;
             } else {
                 console.log('ERROR: Function argument "type" value is not accepted.');
             }
 
-            activeTab.classList.add("hide");
+            activeTab.classList.add('hide');
 
             // buttons to be displayed are based on what the next tab is, i.e. if I clicked the next button whilst on tab 1, then I need to consider what buttons should be shown based on tab 2 - hence adjTabNumber accounts for this
             if (adjTabNumber === 1) {
@@ -83,28 +96,28 @@ function navigateTabs(type) {
 }
 
 function addFormRow(el) {
-    var parentElement = el.parentNode.parentNode
-    var element = document.getElementById(parentElement.id);
-    var elementId = element.id;
-    var elementIdPrefix = getAttributePrefix(elementId);
+    let parentElement = el.parentNode.parentNode
+    let element = document.getElementById(parentElement.id);
+    let elementId = element.id;
+    let elementIdPrefix = getAttributePrefix(elementId);
 
-    var countElements = element.parentNode.querySelectorAll('[id^="' + elementIdPrefix + '"').length;
+    let countElements = element.parentNode.querySelectorAll('[id^="' + elementIdPrefix + '"').length;
 
     // check to make sure that there is at least one row before deleting
     if (countElements < maxRows) {
-        var idToAppendTo = parentElement.parentNode.id;
+        let idToAppendTo = parentElement.parentNode.id;
 
-        var copyHTML = element.cloneNode(true);
+        let copyHTML = element.cloneNode(true);
 
         // see how many of this id already exist and increment it
 
-        var newElementId = elementIdPrefix + parseInt(countElements + 1);
+        let newElementId = elementIdPrefix + parseInt(countElements + 1);
         // change cloned element id to the new incremented value to keep unique
         copyHTML.id = newElementId;
         // execute the clone
         document.getElementById(idToAppendTo).appendChild(copyHTML);
 
-        var newElementChildren = document.getElementById(newElementId);
+        let newElementChildren = document.getElementById(newElementId);
 
         changeAttributeValues(newElementChildren, 'name');
         changeAttributeValues(newElementChildren, 'id');
@@ -114,28 +127,34 @@ function addFormRow(el) {
 }
 
 
-function changeAttributeValues(element, attribute) {
+function changeAttributeValues(parentElement, attribute) {
     /*
     Function is invoked when a new form row is added. It will iterate through new form elements and update the relevant attribute values
     to ensure they remain unique (e.g. name and id)
     */
-    elementAll = element.querySelectorAll('[' + attribute + ']');
-    elementAll.forEach(function (item) {
-        var childElement = item[attribute];
-        var childElementPrefix = getAttributePrefix(childElement);
+    // find all elements under parent that need to be relabelled
+    elements = parentElement.querySelectorAll('[' + attribute + ']');
 
-        var checkElement = document.querySelectorAll('[' + attribute + '^="' + childElementPrefix + '"');
+    elements.forEach(childElement => {
+        let childElementPrefix = getAttributePrefix(childElement[attribute]);
 
-        item[attribute] = childElementPrefix + checkElement.length;
+        // get the number of HTML elements with the same attribute name prefix
+        let countAttributeName = document.querySelectorAll('[' + attribute + '^="' + childElementPrefix + '"');
+
+        // change the attribute value
+        childElement[attribute] = childElementPrefix + countAttributeName.length;
+        // set the new element contents to blank and remove any validation styling
+        childElement.value = "";
+        childElement.classList.remove('success', 'fail');
     });
 }
 
 
 function removeFormRow(element) {
-    var parentDiv = element.parentNode.parentNode;
-    var parentDivIdPrefix = getAttributePrefix(parentDiv.id);
+    let parentDiv = element.parentNode.parentNode;
+    let parentDivIdPrefix = getAttributePrefix(parentDiv.id);
 
-    var countRemainingRows = document.querySelectorAll('[id^="' + parentDivIdPrefix + '"]').length;
+    let countRemainingRows = document.querySelectorAll('[id^="' + parentDivIdPrefix + '"]').length;
 
     // check to make sure that there is at least one row before deleting
     if (countRemainingRows > minRows) {
@@ -154,33 +173,33 @@ function elementReindex() {
     */
 
     // limit the scope of the application to the current tab
-    var parentID = getActiveTab();
+    let parentID = getActiveTab().id;
 
     // element arrays that need to be changed stored in an array to loop through
-    var attributes = ['id', 'name'];
+    let attributes = ['id', 'name'];
 
     // cycle through the element attributes that need to be reindexed
-    var arrayOfAttributes = [];
+    let elements = [];
 
-    attributes.forEach(function (attribute) {
-        var arrayOfElements = document.getElementById(parentID).querySelectorAll('[' + attribute + ']');
+    attributes.forEach(attribute => {
+        let arrayOfElements = document.getElementById(parentID).querySelectorAll('[' + attribute + ']');
 
-        arrayOfElements.forEach(function (element) {
-            var currentElementAttribute = element[attribute];
-            var elementAttributePrefix = getAttributePrefix(currentElementAttribute);
+        arrayOfElements.forEach(element => {
+            let currentElementAttribute = element[attribute];
+            let elementAttributePrefix = getAttributePrefix(currentElementAttribute);
 
-            var ref = "[" + attribute + "]" + elementAttributePrefix;
-            var countOfAttribute = document.getElementById(parentID).querySelectorAll('[' + attribute + '^="' + elementAttributePrefix + '"]').length;
+            let ref = "[" + attribute + "]" + elementAttributePrefix;
+            let countOfAttribute = document.getElementById(parentID).querySelectorAll('[' + attribute + '^="' + elementAttributePrefix + '"]').length;
 
 
-            if (arrayOfAttributes.length === 0) {
+            if (elements.length === 0) {
                 // if there array has no values then set first value
-                pushValuesToArray(arrayOfAttributes, ref, elementAttributePrefix, attribute, countOfAttribute);
+                pushValuesToArray(elements, ref, elementAttributePrefix, attribute, countOfAttribute);
             } else {
                 // if array has at least one value then check the array to make sure the current ref is not already contained
-                var findRefVal = 0;
-                for (i = 0; i < arrayOfAttributes.length; i++) {
-                    if (arrayOfAttributes[i]["ref"] === ref) {
+                let findRefVal = 0;
+                for (let i = 0; i < elements.length; i++) {
+                    if (elements[i]["ref"] === ref) {
                         // if the current ref ([attribute]elementprefix) is found set the value to 1 which means that the values will NOT be pushed to the array and end the loop
                         findRefVal = 1;
                         break;
@@ -189,13 +208,13 @@ function elementReindex() {
 
                 // if the ref was not found then add it to the array
                 if (findRefVal === 0) {
-                    pushValuesToArray(arrayOfAttributes, ref, elementAttributePrefix, attribute, countOfAttribute);
+                    pushValuesToArray(elements, ref, elementAttributePrefix, attribute, countOfAttribute);
                 }
             }
         });
     });
 
-    processElementReindex(arrayOfAttributes);
+    processElementReindex(elements);
 }
 
 function processElementReindex(array) {
@@ -204,19 +223,19 @@ function processElementReindex(array) {
         return;
     }
 
-    for (i = 0; i < array.length; i++) {
-        var attribute = array[i]["attribute"];
-        var attributePrefix = array[i]["prefix"];
-        var elements = document.querySelectorAll('[' + attribute + '^="' + attributePrefix + '"]');
+    for (let i = 0; i < array.length; i++) {
+        let attribute = array[i]["attribute"];
+        let attributePrefix = array[i]["prefix"];
+        let elements = document.querySelectorAll('[' + attribute + '^="' + attributePrefix + '"]');
 
-        elements.forEach(function (item) {
-            var currentAttributeName = item[attribute];
-            var attributeCurrent = array[i]["current"];
-            var newAttributeName = attributePrefix + attributeCurrent;
+        elements.forEach(element => {
+            let currentAttributeName = element[attribute];
+            let attributeCurrent = array[i]["current"];
+            let newAttributeName = attributePrefix + attributeCurrent;
 
             if (currentAttributeName !== newAttributeName) {
-                console.log('Old: ' + item[attribute] + ' ~ New: ' + newAttributeName);
-                item[attribute] = newAttributeName;
+                // console.log('Old: ' + element[attribute] + ' ~ New: ' + newAttributeName);
+                element[attribute] = newAttributeName;
 
             }
             array[i]["current"]++;
@@ -226,20 +245,90 @@ function processElementReindex(array) {
     }
 }
 
-function pushValuesToArray(array, ref, elementName, elementAttribute, count) {
-    array.push({
-        "ref": ref,
-        "prefix": elementName,
-        "attribute": elementAttribute,
-        "count": count,
-        "current": 1
-    });
-}
 
-function getAttributePrefix(attributeValue) {
-    if (typeof (attributeValue) == "string") {
-        var splitStringPosition = parseInt(attributeValue.lastIndexOf('-')) + 1;
 
-        return attributeValue.substr(0, splitStringPosition);
+/* 
+
+FORM CALCULATION FUNCTIONS
+
+*/
+
+
+// function to convert the different form frequencies into a yearly amount
+const getScalar = frequency => {
+    let yearScalar;
+    switch (frequency) {
+        case '7':
+            yearScalar = 52;
+            break;
+        case '14':
+            yearScalar = 26;
+            break;
+        case '28':
+            yearScalar = 13;
+            break;
+        case '30':
+            yearScalar = 12;
+            break;
+        case '60':
+            yearScalar = 6;
+            break;
+        default:
+            yearScalar = 1;
+    };
+    return yearScalar;
+};
+
+const validateText = text => {
+    const pattern = /^[a-zA-Z0-9 ]{1,30}$/
+
+    return pattern.test(text);
+};
+
+const validateNumber = text => {
+    const pattern = /^[0-9]+(\.[0-9][0-9]?)?$/
+
+    return pattern.test(text);
+};
+
+
+// event listener
+const form = document.querySelector('form');
+
+form.addEventListener('change', event => {
+    if (event.target.nodeName === 'INPUT') {
+        if (event.target.id.includes('label')) {
+            if (validateText(event.target.value)) {
+                event.target.classList.add('success');
+                event.target.classList.remove('fail');
+            } else {
+                event.target.classList.add('fail');
+                event.target.classList.remove('success');
+            }
+        } else if (event.target.id.includes('amount')) {
+            if (validateNumber(event.target.value)) {
+                event.target.classList.add('success');
+                event.target.classList.remove('fail');
+            } else {
+                event.target.classList.add('fail');
+                event.target.classList.remove('success');
+            }
+        }
+
+    } else if (event.target.nodeName === 'SELECT') {
+        if (event.target.id.includes('frequency')) {
+            if (event.target.value !== "") {
+                event.target.classList.add('success');
+                event.target.classList.remove('fail');
+            } else {
+                event.target.classList.add('fail');
+                event.target.classList.remove('success');
+            }
+        }
     }
-}
+});
+
+
+
+
+initTab(startTab);
